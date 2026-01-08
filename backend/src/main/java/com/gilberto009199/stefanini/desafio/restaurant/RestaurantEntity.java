b0d2ba5761278a81404b2c952f0c637fdd4598b9
gil <gilberto.tec@vivaldi.net>
2026-01-08 14:17:48 -0300

    add annotation orm in entity

--- backend/src/main/java/com/gilberto009199/stefanini/desafio/restaurant/RestaurantEntity.java
+++ backend/src/main/java/com/gilberto009199/stefanini/desafio/restaurant/RestaurantEntity.java
@@ -1,20 +1,33 @@
 package com.gilberto009199.stefanini.desafio.restaurant;
 
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 
+@Entity
+@Table(name = "tbl_restaurant")
 @Data
 @Builder
 public class RestaurantEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long Id;
+
+    @Column
     private String name;
+
+    @Column
     private String description;
+
+    @Column
     private String cep;
+
+    @Column
     private Float latitude;
+
+    @Column
     private Float longitude;
-
-
 
 }
