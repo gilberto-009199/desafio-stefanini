52d3bb6d7d736de0cc01e2f05768ccf8af50f05e
gil <gilberto.tec@vivaldi.net>
2026-01-08 23:53:09 -0300

    add repsoitory in crud products

--- backend/src/main/java/com/gilberto009199/stefanini/desafio/product/ProductController.java
+++ backend/src/main/java/com/gilberto009199/stefanini/desafio/product/ProductController.java
@@ -1,7 +1,58 @@
 package com.gilberto009199.stefanini.desafio.product;
 
 
+import com.gilberto009199.stefanini.desafio.restaurant.RestaurantEntity;
+import com.gilberto009199.stefanini.desafio.restaurant.RestaurantRepository;
+import com.gilberto009199.stefanini.desafio.restaurant.RestaurantRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("restaurant/{idRestaurant}/product")
 public class ProductController {
 
+    private final ProductRepository repository;
+    private final RestaurantRepository restaurantRepository;
+
+    public ProductController(
+            ProductRepository repository,
+            RestaurantRepository restaurantRepository
+    ) {
+        this.repository = repository;
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductEntity>> get(
+            @PathVariable Long idRestaurant
+    ) {
+        return ResponseEntity.ok(repository.findAllByRestaurantId(idRestaurant));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @PathVariable Long idRestaurant,
+            @RequestBody ProductRequest request,
+            UriComponentsBuilder uriBuilder
+    ) {
+        var entity = ProductEntity.of(request);
+
+        var restaurantEntity = restaurantRepository.findById(idRestaurant).get();
+
+        entity.setRestaurant(restaurantEntity);
+
+        entity = repository.save(entity);
+
+        URI uri = uriBuilder.path("/restaurant/{idRestaurant}/product/{id}")
+                .buildAndExpand(idRestaurant, entity.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(entity.toResponse());
+    }
 
 }
